225a1047e2c36806fcd3b9675033835a1796dfbf
Paweł Żurawka <pzurawka@gmail.com>
2023-07-13 18:45:07 +0200

    feat: generate grid dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,64 @@ import './App.css';
 import { Counter } from './components/Counter';
 import { Text } from './components/Text';
 import { SingleLight } from './components/SingleLight';
+import { Grid } from './components/Grid';
+import { useState } from 'react';
 
 function App() {
+  const [rows, setRows] = useState(0);
+  const [columns, setColumns] = useState(0);
+
+  const handleIncrementRow = () => {
+    setRows(rows + 1);
+  };
+
+  const handleDecrementRow = () => {
+    if (rows <= 0) {
+      return;
+    }
+    setRows(rows - 1);
+  };
+
+  const handleIncrementColumn = () => {
+    if (columns >= 9) {
+      return;
+    }
+    setColumns(columns + 1);
+  };
+
+  const handleDecrementColumn = () => {
+    if (columns <= 0) {
+      return;
+    }
+    setColumns(columns - 1);
+  };
+
+  const totalLights = rows * columns;
+
+  const lights = Array.from({ length: totalLights });
+
   return (
     <div>
       <nav className="nav">
-        <Text>0 bulbs</Text>
+        <Text>{totalLights} bulbs</Text>
         <Counter
-          count={0}
+          onIncrement={handleIncrementRow}
+          onDecrement={handleDecrementRow}
+          count={rows}
           text={'rows'}
         />
         <Counter
-          count={0}
+          onIncrement={handleIncrementColumn}
+          onDecrement={handleDecrementColumn}
+          count={columns}
           text={'columns'}
         />
       </nav>
-      <div>
-        <SingleLight />
-      </div>
+      <Grid columns={columns}>
+        {lights.map((value, i) => {
+          return <SingleLight key={i} />;
+        })}
+      </Grid>
     </div>
   );
 }
