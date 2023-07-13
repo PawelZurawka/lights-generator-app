c876542de006a62614faddd56558d68c12e2ef57
Paweł Żurawka <pzurawka@gmail.com>
2023-07-13 17:24:58 +0200

    feat: create counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import "./App.css";
+import './App.css';
+import { Counter } from './components/Counter';
+import { Text } from './components/Text';
 
 function App() {
-  return <div>Lights generator</div>;
+  return (
+    <div>
+      <nav className="nav">
+        <Text>0 bulbs</Text>
+        <Counter
+          count={0}
+          text={'rows'}
+        />
+        <Counter
+          count={0}
+          text={'columns'}
+        />
+      </nav>
+    </div>
+  );
 }
 
 export default App;
